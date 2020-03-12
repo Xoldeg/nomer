63af4da9be2f29b0c5e593bbeb10af28fae04bf5
b302-08 <b302-08@gmail.com>
2020-03-12 15:52:07 +0300

    comment

---
+++ проверка номера/lib3/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace lib3
+{
+    public class Class1
+    {
+        public bool rav(int x, int y, int z)
+        {
+            if (x==y && y==z)
+            {
+
+            }
+        }
+    }
+}
---
+++ проверка номера/liib2/lib2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace liib2
+{
+    public class lib2
+    {
+
+        public int Raz(int x, int y)
+        {
+            return x - y;
+        }
+    }
+}
