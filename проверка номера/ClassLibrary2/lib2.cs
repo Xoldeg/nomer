7a0d675ec5d472e7a61e5ac8f4f0b91e571bbdf2
b302-08 <b302-08@gmail.com>
2020-03-12 18:20:01 +0300

    комментарий

---
+++ проверка номера/ClassLibrary2/lib2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassLibrary2
+{
+    public class lib2
+    {
+        public int Raz(int x, int y)
+        {
+            return x - y;
+        }
+    }
+}
---
+++ проверка номера/MyCalcLib.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyCalcLib.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyCalcLib.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ec5c015d-7a86-4aae-ab64-2b31b68a8aa7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
