1279e07019588f5a344d091fa1c71554893b648d
b302-08 <b302-08@gmail.com>
2020-03-12 18:14:18 +0300

    комментарий

---
+++ номерАВТО Валеев/Treugol1/Treugol.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Treugol1
+{
+    public class Treugol
+    {
+                public bool number(string car)
+                {
+                    string alpha = "ABEKMHOPCTYX";
+                    string numbers = "1234567890";
+                    int[] regions_from_01_to_99 = Enumerable.Range(01, 99).ToArray();
+                    string y = car.Remove(0, 6);
+                    if ((car[6] == '0' && car[7] == '0') || (car[6] == '0' && car[7] == '0' && car[8] == '0'))
+                    {
+                        return false;
+                    }
+                    else if ((car.Length == 8) && alpha.Contains(car[0]) && alpha.Contains(car[4]) && alpha.Contains(car[5]) && numbers.Contains(car[1]) && numbers.Contains(car[2]) && numbers.Contains(car[3]) && numbers.Contains(car[6]) && numbers.Contains(car[7]) || (car.Length == 9 && alpha.Contains(car[0]) && alpha.Contains(car[4]) && alpha.Contains(car[5]) && numbers.Contains(car[1]) && numbers.Contains(car[2]) && numbers.Contains(car[3]) && ((y == "102") || (y == "113") || (y == "116") || (y == "121") || (y == "123") || (y == "124") || (y == "125") || (y == "126") || (y == "134") || (y == "136") || (y == "138") || (y == "142") || (y == "150") || (y == "152") || (y == "154") || (y == "159") || (y == "161")
+                    || (y == "163") || (y == "164") || (y == "173") || (y == "174") || (y == "177") || (y == "178") || (y == "186") || (y == "190") || (y == "196") || (y == "199") || (y == "750") || (y == "716") || (y == "761") || (y == "763") || (y == "777") || (y == "799"))))
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+    }
+}
---
+++ номерАВТО Валеев/Treugol1Tests/TreugolTests.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Treugol1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treugol1.Tests
+{
+    [TestClass()]
+    public class TreugolTests
+    {
+
+        [TestMethod]
+        public void phoneA444AA22()
+        {
+            string number = "A444AA22";
+            bool expected = true;
+
+            Treugol c = new Treugol();
+            bool actual = c.number(number);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void phoneA444A22()
+        {
+            string number = "А444А22";
+            bool expected = true;
+
+            Treugol c = new Treugol();
+            bool actual = c.number(number);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void phonea444aa22()
+        {
+            string number = "a444aa22";
+            bool expected = true;
+
+            Treugol c = new Treugol();
+            bool actual = c.number(number);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void phone2aaa2222()
+        {
+            string number = "2aaa2222";
+            bool expected = true;
+
+            Treugol c = new Treugol();
+            bool actual = c.number(number);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void phoneA222AA00()
+        {
+            string number = "А222АА00";
+            bool expected = true;
+
+            Treugol c = new Treugol();
+            bool actual = c.number(number);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void phoneA222AA76()
+        {
+            string number = "A222AA76";
+            bool expected = true;
+
+            Treugol c = new Treugol();
+            bool actual = c.number(number);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
